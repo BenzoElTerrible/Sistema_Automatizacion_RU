8ee32c5bf42f7bac42b74c5db0d860be2895b96e
Felipe Espinoza <felipealejandro2960@gmail.com>
2026-06-06 14:46:33 -0400

    Implementa endpoint GET de resoluciones y detalle por ID

--- app/resoluciones/views.py
+++ app/resoluciones/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
+
 from .serializers import ResolucionUniversitariaSerializer
+from .models import ResolucionUniversitaria
 
 
 class ResolucionUniversitariaCreateView(APIView):
@@ -14,3 +16,29 @@ class ResolucionUniversitariaCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResolucionUniversitariaListView(APIView):
+
+    def get(self, request):
+        resoluciones = ResolucionUniversitaria.objects.all()
+        serializer = ResolucionUniversitariaSerializer(
+            resoluciones,
+            many=True
+        )
+        return Response(serializer.data)
+
+
+class ResolucionUniversitariaDetailView(APIView):
+
+    def get(self, request, pk):
+        try:
+            resolucion = ResolucionUniversitaria.objects.get(pk=pk)
+        except ResolucionUniversitaria.DoesNotExist:
+            return Response(
+                {"error": "La resolución no existe"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = ResolucionUniversitariaSerializer(resolucion)
+        return Response(serializer.data)
